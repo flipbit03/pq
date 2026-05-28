e3665459919cd06b899dcca00851152d1ce0f691
ricwo <r@cogr.am>
2026-05-28 13:17:14 +0200

    Release 0.7.2

--- src/pq/__init__.py
+++ src/pq/__init__.py
@@ -5,7 +5,7 @@ from pq.models import Periodic, Task, TaskStatus
 from pq.priority import Priority
 from pq.worker import PostExecuteHook, PreExecuteHook, TaskTimeoutError
 
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 __all__ = [
     "PQ",
